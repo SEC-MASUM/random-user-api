1668d93b4317c560a5e18f6f3c261baeda900343
SEC-MASUM <masum.jess42@gmail.com>
2022-09-16 19:34:08 +0600

    install cors and dotenv and use in index.js

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+require("dotenv").config();
 
 const host = "localhost";
 const port = 5000;
 
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.all("*", (req, res) => {
+  res.send("No route found");
 });
 
 app.listen(port, () => {
