2a423d450d78edb4979ea41e4528b3a78815f608
SEC-MASUM <masum.jess42@gmail.com>
2022-09-17 17:53:33 +0600

    implement saveAUser, updateAUser, deleteAUser controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -54,14 +54,45 @@ module.exports.saveAUser = (req, res, next) => {
     } else {
       res.status(200).send({
         success: true,
-        message: "Successfully user added",
+        message: "Successfully added user",
       });
     }
   });
 };
 
 module.exports.updateAUser = (req, res, next) => {
-  res.send("Update User Api found");
+  const { id } = req.params;
+  const updatedUser = req.body;
+  const oldData = fs.readFileSync(FILENAME);
+  let result = JSON.parse(oldData);
+  let isFoundId = false;
+  result.map((user, index) => {
+    if (user.id === Number.parseInt(id)) {
+      isFoundId = true;
+      result[index] = updatedUser;
+    }
+  });
+  if (isFoundId) {
+    result = JSON.stringify(result);
+    fs.writeFile(FILENAME, result, (err) => {
+      if (err) {
+        res.status(err.code).send({
+          success: false,
+          error: "Internal server error",
+        });
+      } else {
+        res.status(200).send({
+          success: true,
+          message: "Successfully updated user",
+        });
+      }
+    });
+  } else {
+    res.send({
+      success: false,
+      error: "User id not Found",
+    });
+  }
 };
 
 module.exports.updateMultiUser = (req, res, next) => {
@@ -69,5 +100,39 @@ module.exports.updateMultiUser = (req, res, next) => {
 };
 
 module.exports.deleteAUser = (req, res, next) => {
-  res.send("Delete User Api found");
+  const { id } = req.params;
+  const data = fs.readFileSync(FILENAME);
+  let result = JSON.parse(data);
+  let isFoundId = false;
+  let newResult = result.filter((user, index) => {
+    if (user.id !== Number.parseInt(id)) {
+      return true;
+    } else {
+      isFoundId = true;
+      return false;
+    }
+  });
+  // console.log(newResult);
+  if (isFoundId) {
+    newResult = JSON.stringify(newResult);
+    fs.writeFile(FILENAME, newResult, (err) => {
+      if (err) {
+        res.status(err.code).send({
+          success: false,
+          error: "Internal server error",
+        });
+      } else {
+        res.status(200).send({
+          success: true,
+          message: "Successfully deleted user",
+        });
+      }
+    });
+  } else {
+    res.send({
+      success: false,
+      error: "User id not Found",
+    });
+  }
+  // res.send("Delete User Api found");
 };
