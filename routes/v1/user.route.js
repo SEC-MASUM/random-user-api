804cf3741b69b9f342683ca147dd4ab86b4eeeb3
SEC-MASUM <masum.jess42@gmail.com>
2022-09-17 20:21:00 +0600

    add comment

--- routes/v1/user.route.js
+++ routes/v1/user.route.js
@@ -9,7 +9,37 @@ router.route("/all").get(userControllers.getAllUsers);
 router.route("/save").post(userControllers.saveAUser);
 
 router.route("/update/:id").patch(userControllers.updateAUser);
+/**
+[
+  {
+    "id": 1,
+    "data": {
+      "gender": "male",
+      "name": "Masumaaaaaaaaaa",
+      "contact": 1900000000,
+      "address": {
+        "city": "Gwenborough",
+        "zipCode": "92998-3874"
+      },
+      "photoUrl": "https://via.placeholder.com/600/92c952"
+    }
+  },
+  {
+    "id": 2,
+    "data": {
+      "gender": "male",
+      "name": "Tanvir",
+      "address": {
+        "city": "Wisokyburgh",
+        "zipCode": "90566-7771"
+      },
+      "contact": 1711111111,
+      "photoUrl": "https://via.placeholder.com/600/771796"
+    }
+  }
+]
 
+*/
 router.route("/bulk-update").patch(userControllers.updateMultiUser);
 
 router.route("/delete/:id").delete(userControllers.deleteAUser);
