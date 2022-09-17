c2535f995f21471cc109ed2460d2a711f3708dbe
SEC-MASUM <masum.jess42@gmail.com>
2022-09-17 13:06:21 +0600

    implement /all, /random, /save routes controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,13 +1,63 @@
+const fs = require("fs");
+const FILENAME = "./data/user.json";
+
 module.exports.getRandomUser = (req, res, next) => {
-  res.send("Random User Api found");
+  fs.readFile(FILENAME, function (err, data) {
+    if (err) {
+      res.status(err.code).send({
+        success: false,
+        error: "Internal server error",
+      });
+    } else {
+      const result = JSON.parse(data);
+      const randomUser = result[Math.floor(Math.random() * result.length)];
+      console.log(randomUser);
+      res.status(200).send({
+        success: true,
+        message: "Random users found",
+        data: randomUser,
+      });
+    }
+  });
 };
 
 module.exports.getAllUsers = (req, res, next) => {
-  res.send("All User Api found");
+  fs.readFile(FILENAME, function (err, data) {
+    if (err) {
+      res.status(err.code).send({
+        success: false,
+        error: "Internal server error",
+      });
+    } else {
+      const result = JSON.parse(data);
+      res.status(200).send({
+        success: true,
+        message: "All users found",
+        data: result,
+      });
+    }
+  });
 };
 
 module.exports.saveAUser = (req, res, next) => {
-  res.send("Save User Api found");
+  const newUser = req.body;
+  const oldData = fs.readFileSync(FILENAME);
+  let result = JSON.parse(oldData);
+  result.push(newUser);
+  result = JSON.stringify(result);
+  fs.writeFile(FILENAME, result, (err) => {
+    if (err) {
+      res.status(err.code).send({
+        success: false,
+        error: "Internal server error",
+      });
+    } else {
+      res.status(200).send({
+        success: true,
+        message: "Successfully user added",
+      });
+    }
+  });
 };
 
 module.exports.updateAUser = (req, res, next) => {
